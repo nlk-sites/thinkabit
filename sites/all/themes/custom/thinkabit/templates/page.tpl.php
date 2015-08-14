61dde6828044848e9a47723b315d0d5e14690489
Troy Shields <troyshields@Troys-13-MacBook-Pro.local>
2015-08-14 18:44:15 -0400

    QTBL-79 Udpating styles for views and detail pages, taxonomy view

--- sites/all/themes/custom/thinkabit/templates/page.tpl.php
+++ sites/all/themes/custom/thinkabit/templates/page.tpl.php
@@ -95,6 +95,11 @@
     <div class="l-content" role="main">
       <?php print render($page['highlighted']); ?>
       <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
